51c0a3a5caa32025fc275cb62e64e8e38146d739
gh1 <vibone@126.com>
2015-04-03 16:01:17 +0800

    添加

---
+++ return和finally/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace return和finally
+{
+    /// <summary>
+    /// 如果try语句里有return，那么代码的行为如下：
+    ///1.如果有返回值，就把返回值保存到局部变量中
+    ///2.执行jsr指令跳到finally语句里执行
+    ///3.执行完finally语句后，返回之前保存在局部变量表里的值
+    /// </summary>
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var y = aaa();
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+
+        public static int aaa()
+        {
+            var x = 1;
+            try
+            {
+                return ++x;
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                ++x;
+            }
+            return x;
+        }
+    }
+}
